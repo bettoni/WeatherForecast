58101579979548083213ddb5ad472d58ece2f2a5
Fernando Bettoni <fernandobettoni@gmail.com>
2019-01-27 12:01:19 -0200

    Changed annotations

--- src/main/java/io/bettoni/weatherForecast/forecast/ForecastController.java
+++ src/main/java/io/bettoni/weatherForecast/forecast/ForecastController.java
@@ -4,11 +4,11 @@ import io.bettoni.weatherForecast.forecast.domain.ForecastFilter;
 import io.bettoni.weatherForecast.forecast.domain.ForecastResult;
 import io.bettoni.weatherForecast.forecast.domain.ForecastService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ForecastController {
 
     private ForecastService forecastService;
@@ -18,7 +18,7 @@ public class ForecastController {
         this.forecastService = forecastService;
     }
 
-    @GetMapping(value = "/data/{country}/{city}")
+    @RequestMapping("/data/{country}/{city}")
     public ForecastResult getForecastFor(@PathVariable String country, @PathVariable String city) {
         return forecastService.getData(createFilterFrom(country, city));
     }
--- src/main/java/io/bettoni/weatherForecast/forecast/domain/ForecastService.java
+++ src/main/java/io/bettoni/weatherForecast/forecast/domain/ForecastService.java
@@ -1,5 +1,8 @@
 package io.bettoni.weatherForecast.forecast.domain;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ForecastService {
 
     public ForecastResult getData(ForecastFilter cityName) {
